1049b250e66e9ac70bc7068288ef85ab2e6ce032
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-11-18 16:59:26 -0800

    first commit

---
+++ src/GolestanView/Controller.java
@@ -0,0 +1,30 @@
+package GolestanView;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class Controller {
+
+    @FXML
+    private AnchorPane Firstpane;
+
+    @FXML
+    private Separator sepUni;
+
+    @FXML
+    private Label lbl2;
+
+    @FXML
+    private Label lbl3;
+
+    @FXML
+    private Button entrancebtn;
+
+    @FXML
+    private ImageView logoimg;
+
+}
