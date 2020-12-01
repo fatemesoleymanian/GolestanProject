2469274fb5cbd7db2a886bf0670fa03a6d78ded5
fatemesoleymanian <71789169+fatemesoleymanian@users.noreply.github.com>
2020-12-01 13:30:18 -0800

    second

--- src/GolestanController/FirstPage.java
+++ src/GolestanController/FirstPage.java
@@ -1,11 +1,18 @@
 package GolestanController;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+
+
+import java.awt.*;
+import java.net.URI;
+
 
 public class FirstPage {
 
@@ -26,5 +33,22 @@ public class FirstPage {
 
     @FXML
     private JFXButton entrancebtn;
+    @FXML
+    private Hyperlink contactUshyperlink;
 
+
+    @FXML
+    public void pressHyperLink(ActionEvent actionEvent) {
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI oURL = new URI("www.usc.ac.ir");
+            desktop.browse(oURL);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void pressentrancebtn(ActionEvent actionEvent) {
+    }
 }
+
